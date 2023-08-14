db2ce682989c138d77c0f800ee894bd5ecb7a839
Vivek Kumar <vivekkr0311@gmail.com>
2023-08-14 17:01:29 +0530

    Screenshot and Logger Added

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -2,24 +2,38 @@ import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
 
 class Test_001_Login:
     baseURL = ReadConfig.getApplicationURL()
     username = ReadConfig.getUserName()
     password = ReadConfig.getUserPassword()
 
+    logger = LogGen.logGen()
+
+    logger.info("********** Test_001 Login ***********")
     def test_homePageTitle(self, setup):
+        self.logger.info("********** Verifying Home Page Title ***********")
+
         # Here, setup is fixture which is used to launch the webdriver.
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
-        self.driver.close()
+
         if act_title == "Your store. Login":
-           assert True
+            assert True
+            self.driver.close()
+            self.logger.info("********** Title Validation Passed ***********")
         else:
+            self.driver.save_screenshot(
+                "/Users/vivek/PycharmProjects/Python_Hybrid_Automation_Framework/Screenshots/" + "test_HomePageTitle.png")
             assert False
+            self.driver.close()
+            self.logger.error("********** Title Validation Failed ***********")
 
     def test_login(self, setup):
+        self.logger.info("********** Verifying Log In Test Case ***********")
+
         # Here, setup is fixture which is used to launch the webdriver.
         self.driver = setup
         self.driver.get(self.baseURL)
@@ -30,8 +44,13 @@ class Test_001_Login:
         self.loginPage_Object.clickLogin()
 
         act_title = self.driver.title
-        self.driver.close()
         if act_title == "Dashboard / nopCommerce administration":
-           assert True
+            assert True
+            self.driver.close()
+            self.logger.info("********** Test Log In Validation Passed ***********")
         else:
+            self.driver.save_screenshot(
+                "/Users/vivek/PycharmProjects/Python_Hybrid_Automation_Framework/Screenshots/" + "test_HomePageTitle.png")
             assert False
+            self.driver.close()
+            self.logger.error("********** Test Log in Validation Failed ***********")
---
+++ utilities/customLogger.py
@@ -0,0 +1,23 @@
+import logging  # Import the logging module to enable logging functionality.
+
+class LogGen:
+
+    @staticmethod
+    def logGen():
+        # Configure the logging settings using the basicConfig function.
+        # This configures the root logger for the entire application.
+        logging.basicConfig(
+            filename="/Users/vivek/PycharmProjects/Python_Hybrid_Automation_Framework/Logs/automation.log",  # Set the file path where log messages will be saved.
+            format='%(asctime)s: %(levelname)s: %(message)s',  # Set the format of each log message.
+            datefmt='%m/%d/%Y %I:%M:%S %p'  # Set the date and time format for log messages.
+        )
+        
+        # Get the root logger instance.
+        logger = logging.getLogger()
+
+        # Set the logging level for the root logger to INFO.
+        # This means that log messages with severity INFO and above will be captured.
+        logger.setLevel(logging.INFO)
+
+        # Return the configured logger instance to be used for logging in other parts of the code.
+        return logger
